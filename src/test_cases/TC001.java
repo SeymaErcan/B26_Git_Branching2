0ba41efb5521b63809f272cbf5293b37d165546f
Seyma <seyma.ercan@gmail.com>
2022-04-08 19:08:29 -0400

    updated tc001

--- src/test_cases/TC001.java
+++ src/test_cases/TC001.java
@@ -3,7 +3,9 @@ package test_cases;
 public class TC001 {
     public static void main(String[] args) {
 
-        //testing some thing
+        // testing some thing
+
+        // more testing
 
     }
 }
